cae5c79bfb09f6ee3b91d78d842288cafb92d083
Germn <germangodoygutierrez@gmail.com>
2021-04-29 18:45:16 +0200

    Added follow redirects and color

--- main.go
+++ main.go
@@ -9,11 +9,32 @@ import (
 	"strings"
 )
 
-func makeConnection(url string) (status, charas, lines int){
 
-	fmt.Printf("[*] Starting the petiton to %s", url)
+var Reset  = "\033[0m"
+var Red    = "\033[31m"
+var Green  = "\033[32m"
+var Yellow = "\033[33m"
+var Blue   = "\033[34m"
+var Purple = "\033[35m"
+var Cyan   = "\033[36m"
+var Gray   = "\033[37m"
+var White  = "\033[97m"
+
+
+
+
+func makeConnection(url string, redirect bool) (status, charas, lines int, finalurl string){
+
+	fmt.Printf("%s[*]%s Starting the petiton to %s", Yellow, Reset, url)
 
 	client := &http.Client{}
+
+	if !redirect {
+		client = &http.Client{
+			CheckRedirect: func(req *http.Request, via []*http.Request) error {
+				return http.ErrUseLastResponse
+			}}
+	}
 
 	req, err := http.NewRequest("GET", url, nil)
 
@@ -40,7 +61,9 @@ func makeConnection(url string) (status, charas, lines int){
 	lines = strings.Count(bod, "\n")
 	lines = lines + 1
 
-	return status, charas, lines
+	finalurl = resp.Request.URL.String()
+
+	return status, charas, lines, finalurl
 }
 
 
@@ -48,10 +71,16 @@ func main(){
 
 	url := flag.String("url", "https://en.wikipedia.org/wiki/%22Hello,_World!%22_program", "The url you want to do the attack to :)")
 
+	redirect := flag.Bool("redirect", false, "This will tell the fuzzing to follow redirects")
+
 	flag.Parse()
 
-	status, charas, lines := makeConnection(*url)
+	status, charas, lines, finalurl := makeConnection(*url, *redirect)
 
-	fmt.Printf("[*] PETITION INFORMATION\n  -Status: %d %s\n  -Characters: %d\n  -Lines: %d", status, http.StatusText(status), charas, lines)
+	fmt.Println()
+
+	fmt.Printf("%s[*]%s PETITION INFORMATION\n  -Status: %d %s\n  -Characters: %d\n  -Lines: %d  -URL: %s",Yellow, Reset, status, http.StatusText(status), charas, lines, finalurl)
+
+	fmt.Println()
 }
 
